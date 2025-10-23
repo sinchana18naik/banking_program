95f797c5c618455b3cfde8a9bba505f811610f4d
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-23 15:46:53 +0530

    save to new branch

--- bank.py
+++ bank.py
@@ -1,6 +1,10 @@
 initial_balance = float(input("Enter initial balance (₹): "))
 deposit = float(input("Enter deposit amount (₹): "))
 new_balance = initial_balance + deposit
+withdraw = float(input("enter withdrawal amount:"))
+final_balance = new_balance - withdraw
 print("\nInitial Balance: ₹", initial_balance)
 print("Deposit: ₹", deposit)
 print("New Balance after deposit: ₹", new_balance)
+print("withdraw:",withdraw)
+print("final balance :",final_balance)
